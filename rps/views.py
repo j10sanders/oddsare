e2db0fd14a6c1f41d34231cb997f92d3cd6b1ba5
j10sanders <jps458@nyu.edu>
2015-12-26 20:33:20 +0000

    game is working, players can't change moves (games are saved), but no login added

--- rps/views.py
+++ rps/views.py
@@ -1,43 +1,18 @@
-from flask import Flask, flash, render_template, request, redirect, url_for
-from rps import app
+from flask import Flask, flash, render_template, request, redirect, url_for, abort
+from rps import app, rps
 
 from .database import Game, session
 import os
 
 
-'''@app.route("/compare", methods="GET")
-def compare(moves):
-    moves = session.query(Entry)
-    entries = session.query(Entry)
-        entries = entries.order_by(Entry.datetime.desc())
-    if moves[0] == moves[1]:
-        return None
-    elif moves == ["rock", "paper"]:
-        return 1
-    elif moves == ["rock", "scissors"]:
-        return 0
-    elif moves == ["scissors", "rock"]:
-        return 1
-    elif moves == ["scissors", "paper"]:
-        return 0
-    elif moves == ["paper", "scissors"]:
-        return 1
-    elif moves == ["paper", "rock"]:
-        return 0
-            compare.moves = [player1, player2]
-    if compare.moves == 0:
-        print("Player 1 wins!")
-    elif compare.moves == 1:
-        print("Player 2 wins!")
-    else:
-        print("You tied!")'''
-
-
-#@app.route("/")
+@app.route("/")
+@app.route("/game", methods=["GET"])
 @app.route("/new_game", methods=["GET"])
 def player1_choice_get():
     return render_template("player1.html")
 
+@app.route("/", methods=["POST"])
+@app.route("/game", methods=["POST"])
 @app.route("/new_game", methods=["POST"])
 def player1_choice():
     possibilities = ["rock", "paper", "scissors"]
@@ -54,23 +29,33 @@ def player1_choice():
 @app.route("/game/<id>", methods=["GET"])
 def player2_choice_get(id):
     # loads the game by id
-    game = Game.query.get(id)
+    game = session.query(Game).get(id)
+    if game is None:
+        abort(404)
+    if game.move2:
+        result = rps.compare([game.move1, game.move2])
+        return render_template("result.html", game=game, result=result)
     # check if move2 is defined.
     # if it has been played, display the result
-    return render_template("player2.html", game=game)
+    else:
+        return render_template("player2.html", game=game)
 
 @app.route("/game/<id>", methods=["POST"])
-def player2_choice():
+def player2_choice(id):
+    game = session.query(Game).get(id)
     possibilities = ["rock", "paper", "scissors"]
     move = request.form["move"]
     if move not in possibilities:
         flash("That's not a possibility.  Please choose from " + str(possibilities)[1:-1])
-        return redirect(url_for("player2_choice_get"))
-    else:
-        player_move = Player(
-            name="Player2",
-            move=request.form["move"]
-            )
-        session.add(player_move)
+        new_game = Game(move1=request.form["move"])
+        session.add(new_game)
         session.commit()
-        return redirect(url_for("compare"))
+        return redirect(url_for("player2_choice_get", id=new_game.id))
+    else:
+        game.move2=request.form["move"]
+        session.add(game)
+        session.commit()
+        result = rps.compare([game.move1, game.move2])
+        return render_template("result.html", game=game, result=result)
+        
+    
