2fe6c4bbe440d4c9b0cdb3b4c60c8449927f869f
j10sanders <jps458@nyu.edu>
2016-01-06 01:21:12 +0000

    removed rps

--- rps/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from flask import Flask
-
-app = Flask(__name__)
-config_path = os.environ.get("CONFIG_PATH", "rps.config.DevelopmentConfig")
-app.config.from_object(config_path)
-
-from . import views
--- rps/test_game.py
+++
@@ -1,7 +0,0 @@
-from game import compare
-
-def test_compare():
-    assert compare(["rock", "paper"]) == 1
-    assert compare(["paper", "paper"]) is None
-    assert compare(["rock", "scissors"]) == 0
-    
